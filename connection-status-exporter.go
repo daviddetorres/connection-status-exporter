aff93e6deefece7d9f3cbd31d5f0f38196554393
David de Torres <detorres.david@gmail.com>
2019-11-23 23:40:30 +0100

    First functional version
    - First functional version checking a set of sockets and exporting the status of the connection as metrics

--- connection-status-exporter.go
+++ connection-status-exporter.go
@@ -15,23 +15,39 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"io/ioutil"
 	"log"
+	"net"
+	"net/http"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"gopkg.in/yaml.v2"
 )
 
 const (
 
 	// Prefix for Prometheus metrics
-	namespace = "connection_status"
+	namespace = "connection_status_up"
 
-	// Default timeout in seconds
-	defaultTimeout = 5
+	// Default values for optional parameters of socket
+	defaultTimeout         = 5
+	defaultProtocol string = "tcp"
+
+	// Constant values
+	connectionOk  = 1
+	connectionErr = 0
+)
+
+var (
+	logger     = log.New(os.Stderr, "", log.Lmicroseconds|log.Ltime|log.Lshortfile)
+	configFile = flag.String("config-file", "config/config.yaml", "Exporter configuration file.")
+	addr       = flag.String("listen-address", ":8888", "The address to listen on for HTTP requests.")
 )
 
 type socketSet struct {
@@ -43,18 +59,18 @@ type socket struct {
 	Host     string `yaml:"host"`
 	Port     string `yaml:"port"`
 	Protocol string `yaml:"protocol"`
+	Timeout  int    `yalm:"timeout"`
 }
 
 // SocketSetExporter Exporter of the status of connection
 type SocketSetExporter struct {
-	statusMetrics *prometheus.GaugeVec
-	mutex         sync.Mutex
-	sockets       *socketSet
+	socketStatusMetrics *prometheus.GaugeVec
+	mutex               sync.Mutex
+	sockets             *socketSet
 }
 
 // NewSocketSetExporter Creator of SocketSetExporter
 func NewSocketSetExporter(configFile string) *SocketSetExporter {
-
 	yalmFile, err := ioutil.ReadFile(configFile)
 	if err != nil {
 		logger.Fatalf("Error while reading configuration file: &%s", err)
@@ -65,38 +81,117 @@ func NewSocketSetExporter(configFile string) *SocketSetExporter {
 	if err != nil {
 		logger.Fatalf("Error parsing config file: %s", err)
 	}
+
+	err = socketSet.check()
+	if err != nil {
+		logger.Fatalf("Error in the configuration of the sockets: %s", err)
+	}
+
 	return &SocketSetExporter{
-		statusMetrics: prometheus.NewGaugeVec(
+		socketStatusMetrics: prometheus.NewGaugeVec(
 			prometheus.GaugeOpts{
 				Name: namespace,
 				Help: "Connection status of the socket.",
-			}, []string{"host", "port", "protocol"}),
+			}, []string{"name", "host", "port", "protocol"}),
 		sockets: &socketSet,
 	}
-
-}
-
-// Init Initialize the statusMetrics
-func (exporter *SocketSetExporter) Init() {
-	prometheus.MustRegister(exporter.statusMetrics)
 }
 
 // Describe Implements interface
-func (exporter *SocketSetExporter) Describe(ch chan<- *prometheus.Desc) {
-
+func (exporter *SocketSetExporter) Describe(prometheusChannel chan<- *prometheus.Desc) {
+	exporter.socketStatusMetrics.Describe(prometheusChannel)
+	return
 }
 
-var (
-	logger     = log.New(os.Stderr, "", log.Lmicroseconds|log.Ltime|log.Lshortfile)
-	configFile = flag.String("config-file", "config/config.yaml", "Exporter configuration file.")
-)
+// Collect Implements interface
+func (exporter *SocketSetExporter) Collect(prometheusChannel chan<- prometheus.Metric) {
+	exporter.mutex.Lock()
+	defer exporter.mutex.Unlock()
+	exporter.sockets.collect(exporter.socketStatusMetrics)
+	exporter.socketStatusMetrics.Collect(prometheusChannel)
+	return
+}
+
+// Calls the method collect of each socket in the socketSet
+func (thisSocketSet *socketSet) collect(prometheusGaugeVector *prometheus.GaugeVec) {
+	for _, currentSocket := range thisSocketSet.Sockets {
+		currentSocket.collect(prometheusGaugeVector)
+	}
+	return
+}
+
+// Checks the status of the connection of a socket and updates it in the Metric
+func (thisSocket *socket) collect(prometheusGaugeVector *prometheus.GaugeVec) {
+	connectionStatus := connectionOk
+	connectionAdress := thisSocket.Host + ":" + thisSocket.Port
+	connectionTimeout := time.Duration(thisSocket.Timeout * 1000000000)
+
+	// Create a connection to test the socket
+	connection, err := net.DialTimeout(
+		thisSocket.Protocol,
+		connectionAdress,
+		connectionTimeout)
+
+	// If the socket cannot be opened, set the status to error
+	if err != nil {
+		connectionStatus = connectionErr
+	}
+
+	// Updated the status of the socket in the metric
+	prometheusGaugeVector.WithLabelValues(
+		thisSocket.Name,
+		thisSocket.Host,
+		thisSocket.Port,
+		thisSocket.Protocol).Set(float64(connectionStatus))
+
+	// If the socket was open correctly, close it
+	if connectionStatus == connectionOk {
+		err = connection.Close()
+		if err != nil {
+			logger.Printf("Error closing the socket")
+		}
+	}
+	return
+}
+
+// check the sanity of the sockets in the set
+func (thisSocketSet *socketSet) check() error {
+	for index := range thisSocketSet.Sockets {
+		err := thisSocketSet.Sockets[index].check()
+		if err != nil {
+			return (err)
+		}
+	}
+	return (nil)
+}
+
+// Check the sanity of the socket and fills the default values
+func (thisSocket *socket) check() error {
+	if thisSocket.Name == "" {
+		return (errors.New("All sockets must have the field name completed"))
+	}
+	if thisSocket.Name == "" {
+		return (errors.New("All sockets must have the fiels host completed"))
+	}
+	if thisSocket.Port == "" {
+		return (errors.New("All sockets must have the field port completed"))
+	}
+	if thisSocket.Protocol == "" {
+		thisSocket.Protocol = defaultProtocol
+	}
+	if thisSocket.Timeout == 0 {
+		thisSocket.Timeout = defaultTimeout
+	}
+	return (nil)
+}
 
 func main() {
 	flag.Parse()
-
 	exporter := NewSocketSetExporter(*configFile)
-
-	exporter.Init()
-
+	prometheus.MustRegister(exporter)
 	logger.Print("Socket exporter initialized.")
+
+	// Expose the registered metrics via HTTP.
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
